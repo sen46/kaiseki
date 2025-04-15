2b908e4578a76ed0d0fdfac304ab81f203104d37
sen46 <sw20050719@gmail.com>
2025-04-15 13:28:36 +0900

    added 例題1

---
+++ 01/example01.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <vector>
+
+void example1();
+
+int main()
+{
+    example1();
+    return (0);
+}
+
+void example1()
+{
+    std::vector<double> a(31);
+
+    a[0] = 1;
+    for (int i = 1; i <= 30; i++)
+    {
+        a[i] = a[i - 1] + 1 / (double)(i);
+    }
+    printf("%.14e\n", a[30]);
+}
