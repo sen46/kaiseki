3d54536b10fd8b186690e657b583714cad077d9c
sen46 <sw20050719@gmail.com>
2025-04-21 21:26:29 +0900

    first commit

---
+++ 02/self_study/ex02.cpp
@@ -0,0 +1,32 @@
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <time.h>
+#include <cmath>
+
+int main()
+{
+    srand(time(NULL));
+    int     d = rand() % 80 + 20;
+    double  sum = 0;
+    double  sum2 = 0;
+
+    std::vector<int> vec(d);
+    for (int i = 0; i < d; i++)
+    {
+        vec[i] = rand() % 100;
+    }
+
+    for (int i = 0; i < d; i++)
+    {
+        sum += vec[i];
+        vec[i] *= vec[i];
+        sum2 += vec[i];
+    }
+
+    // 2乗の平均 - 平均の2乗
+    sum = (sum / d) * (sum / d);
+    sum2 /= d;
+
+    printf("標準偏差は%lfです\n", sqrt(sum2 - sum));
+}
