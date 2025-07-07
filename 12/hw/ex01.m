9d11a8f4feedfbb61acaabda692be92c4dd98eaa
sen46 <sw20050719@gmail.com>
2025-07-07 16:41:42 +0900

    initial commit

---
+++ 12/hw/ex01.m
@@ -0,0 +1,37 @@
+% function U = ex01(n, m)
+
+	n = 50;
+	m = 5000;
+	% 位置の刻み幅
+	h = 1 / n;
+	% 時間の刻み幅
+	k = 1 / m;
+	r = k / h ^ 2;
+	% n + 1行, m + 1列の行列
+	% 位置、時間
+	U = zeros(n + 1, m + 1);
+
+	% 時間が0のときの初期条件
+	for i = 2 : n
+		x = (i - 1) * h;
+		U(i, 1) = sin(2 * pi * x);
+	end
+	U(1, 1) = 0;
+	U(n + 1, 1) = 0;
+
+	% t = 0のときは上で初期化済みだからj = 2スタート
+	for j = 1 : m
+		for i = 1 : n + 1
+			if (i == 1 || i == n + 1)
+				U(i, j) = 0;
+			else
+				U(i, j + 1) = r * U(i - 1, j) + (1 - 2 * r) * U(i, j) + r * U(i + 1, j);
+			end
+		end
+	end
+
+	figure(1)
+	clf
+	plot(linspace(0, 1, n + 1), U(:, m + 1));
+	xlabel('x-axis')
+	ylabel('y-axis')
---
+++ 12/hw/ex02.m
@@ -0,0 +1,41 @@
+% function U = ex02(n, m)
+
+	n = 100;
+	m = 50000;
+	% 位置の刻み幅
+	h = 1 / n;
+	% 時間の刻み幅
+	k = 1 / m;
+	r = k / h ^ 2;
+	% n + 1行, m + 1列の行列
+	% 位置、時間
+	U = zeros(n + 1, m + 1);
+
+	% 時間が0のときの初期条件
+	for i = 2 : n
+		x = (i - 1) * h;
+		if (x <= 1 / 2)
+			U(i, 1) = 1000;
+		else
+			U(i, 1) = 0;
+		end
+	end
+	U(1, 1) = 0;
+	U(n + 1, 1) = 0;
+
+	% t = 0のときは上で初期化済みだからj = 2スタート
+	for j = 1 : m
+		for i = 1 : n + 1
+			if (i == 1 || i == n + 1)
+				U(i, j) = 0;
+			else
+				U(i, j + 1) = r * U(i - 1, j) + (1 - 2 * r) * U(i, j) + r * U(i + 1, j);
+			end
+		end
+	end
+
+	figure(2)
+	clf
+	plot(linspace(0, 1, n + 1), U(:, m + 1));
+	xlabel('x-axis')
+	ylabel('y-axis')
